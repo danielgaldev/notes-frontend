be8b75b5f7227abd90b1ed05fbe0a31dfedb0696
Daniel Gal <daniel.gal@sch.bme.hu>
2019-02-27 23:38:29 +0100

    Reducers, store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import NotFound from './components/NotFound';
 import DenNote from './components/DenNote';
+import denApp from "./reducers";
 
-
+let store = createStore(denApp);
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={DenNote} />
-        <Route component={NotFound} />
-      </Switch>
-      </BrowserRouter>
-    )
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={DenNote} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/DenNote.js
+++ src/components/DenNote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux';
 
 
 class DenNote extends Component {
@@ -7,12 +8,36 @@ class DenNote extends Component {
     return (
       <div>
         <h2 className="ui header">Welcome to PonyNote!</h2>
-        <p>
-        <Link to="/contact">Click Here</Link> to contact us!
-        </p>
+        <hr />
+
+        <table className="ui collapsing celled table">
+            <tbody>
+                {this.props.notes.map(note => (
+                    <tr>
+                        <td>{note.text}</td>
+                        <td><button className="ui button">Edit</button></td>
+                        <td><button className="ui button">Delete</button></td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        
       </div>
     )
   }
 }
 
-export default DenNote
+const mapStateToProps = state => {
+  return {
+      notes: state.notes,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(DenNote);
