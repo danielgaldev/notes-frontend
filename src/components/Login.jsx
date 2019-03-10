430d42c617b26beb752ae86ef2b58a69d74f7bdf
Daniel Gal <daniel.gal@sch.bme.hu>
2019-03-10 13:51:56 +0100

    Working pretty login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,81 +1,76 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
 
-import {Link, Redirect} from "react-router-dom";
-
-import {auth} from "../actions";
+import { auth } from "../actions";
 
 class Login extends Component {
 
-    state = {
-        username: "",
-        password: "",
-    }
+  state = {
+    username: "",
+    password: "",
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
-        this.props.login(this.state.username, this.state.password);
-    }
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.login(this.state.username, this.state.password);
+  }
 
-    render() {
-        if (this.props.isAuthenticated) {
-            return <Redirect to="/" />
-        }
-        return (
-            <form onSubmit={this.onSubmit}>
-                <fieldset>
-                    <legend>Login</legend>
-                    {this.props.errors.length > 0 && (
-                        <ul>
-                            {this.props.errors.map(error => (
-                                <li key={error.field}>{error.message}</li>
-                            ))}
-                        </ul>
-                    )}
-                    <p>
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="text" id="username"
-                            onChange={e => this.setState({username: e.target.value})} />
-                    </p>
-                    <p>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password" id="password"
-                            onChange={e => this.setState({password: e.target.value})} />
-                    </p>
-                    <p>
-                        <button type="submit">Login</button>
-                    </p>
-
-                    <p>
-                        Don't have an account? <Link to="/register">Register</Link>
-                    </p>
-                </fieldset>
-            </form>
-        )
+  render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to="/" />
     }
+    return (
+      <div class="ui container segment">
+        <form class="ui form" onSubmit={this.onSubmit}>
+          {this.props.errors.length > 0 && (
+            <div>
+              {this.props.errors.map(error => (
+                <div class="ui visible warning message" key={error.field}>
+                  {error.message}
+                </div>
+              ))}
+            </div>
+          )}
+          <div class="field">
+            <label>Username</label>
+            <input type="text" id="username" placeholder="Username"
+              onChange={e => this.setState({ username: e.target.value })} />
+          </div>
+          <div class="field">
+            <label>Password</label>
+            <input type="password" id="password" placeholder="Password"
+              onChange={e => this.setState({ password: e.target.value })} />
+          </div>
+          <button class="ui button" type="submit">Login</button>
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </form>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    let errors = [];
-    if (state.auth.errors) {
-        errors = Object.keys(state.auth.errors).map(field => {
-            return {field, message: state.auth.errors[field]};
-        });
-    }
-    return {
-        errors,
-        isAuthenticated: state.auth.isAuthenticated
-    };
+  let errors = [];
+  if (state.auth.errors) {
+    errors = Object.keys(state.auth.errors).map(field => {
+      return { field, message: state.auth.errors[field] };
+    });
+  }
+  return {
+    errors,
+    isAuthenticated: state.auth.isAuthenticated
+  };
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        login: (username, password) => {
-            return dispatch(auth.login(username, password));
-        }
-    };
+  return {
+    login: (username, password) => {
+      return dispatch(auth.login(username, password));
+    }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
